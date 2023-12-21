64c26e5f418785ba7fe8f66ab5a7905118e535f9
Caio Lima <107970067+caiolimasantos@users.noreply.github.com>
2023-12-21 15:34:55 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,9 +1,9 @@
 <?php 
-    $host = "dk.athenasacademy.com.br";
+    $host = "";
     $port = 3306;
-    $username = "athena85_sistemacurso";
-    $password = "417782Ja@";
-    $database = "athena85_sistemacurso";
+    $username = "";
+    $password = "";
+    $database = "";
 
     $strcon = mysqli_connect($host, $username, $password, $database, $port);
 
